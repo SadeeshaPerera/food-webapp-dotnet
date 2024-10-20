7f346ee39a2242b2e133abd9b7682bff272b2c52
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-10-20 10:49:00 +0530

    Add project files.

---
+++ Data/MenuContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using food_webapp_dotnet.Models;
+
+namespace food_webapp_dotnet.Data
+{
+	public class MenuContext : DbContext
+	{
+		public MenuContext(DbContextOptions<MenuContext> options):base(options)  { }
+	}
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<DishIngredient>()
+			.HasKey(di => new { di.DishId, di.IngredientId });
+		modelBuilder.Entity<DishIngredient>()
+			.HasOne(di => di.Dish)
+			.WithMany(d => d.DishIngredients)
+			.HasForeignKey(di => di.DishId);
+		modelBuilder.Entity<DishIngredient>()
+			.HasOne(di => di.Ingredient)
+			.WithMany(i => i.DishIngredients)
+			.HasForeignKey(di => di.IngredientId);
+	}
+}
---
+++ Models/Dish.cs
@@ -0,0 +1,16 @@
+﻿namespace food_webapp_dotnet.Models
+{
+	public class Dish
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		
+		public string ImageUrl { get; set; }
+		public double Price { get; set; }
+		public List<DishIngredient>? DishIngredients { get; set; }
+
+
+
+
+	}
+}
