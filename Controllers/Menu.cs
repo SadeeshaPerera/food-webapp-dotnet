42d9fba0da504b0a8d0aaa8c1b1e388febd249a0
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-10-20 13:09:10 +0530

    chore:Implement views

---
+++ Controllers/Menu.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using food_webapp_dotnet.Data;
+using food_webapp_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace food_webapp_dotnet.Controllers
+{
+	public class Menu : Controller
+	{
+	    private readonly MenuContext _context;
+		public Menu(MenuContext context)
+		{
+			_context = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			return View( await _context.Dishes.ToListAsync());
+		}
+	}
+}
